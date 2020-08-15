253b3530a511d4b4c19d7167f6fe93c05ddaba11
vedavyas.k <vedavyas.k17@iiits.in>
2020-08-16 04:38:41 +0530

    non-refactored code

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express') ;
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express() ;
+const keys = require('./config/keys.js')
 
-app.get('/' , (req,res) => {
-    res.send({'veda':'vyas'}) ;
-});
+
+passport.use(new GoogleStrategy(
+{
+    clientID : keys.googleClientID ,
+    clientSecret : keys.googleClientSecret ,
+    callbackURL : '/auth/google/callback'
+}
+ , (acessToken , refreshToken , profile , done)=> {
+    console.log('acessToken' , acessToken);
+    console.log('refreshToken' , refreshToken);
+    console.log('profile' , profile);
+ }));
+
+app.get('/auth/google' , passport.authenticate('google' , { scope : ['profile' , 'email'] }))
+
+
+app.get('/auth/google/callback' , passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000 ;
 
